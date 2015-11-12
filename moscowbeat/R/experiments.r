f731980afec3895e337c5a1e0fca17c84dec691b
Александр Демиденко <a.demidenko@mrcur.local>
2015-11-12 18:43:56 +0300

    Updated r file. 3sigma need to be done

--- moscowbeat/R/experiments.r
+++ moscowbeat/R/experiments.r
@@ -1,18 +1,44 @@
 library(cluster)
 library(fpc)
 
+treesigmareduce <- function(x)
+{
+  if(!is.array(x))
+  {
+    return(x);
+  }
+  
+  cols <- ncol(x);
+  
+  data <- array(, ñ());
+  
+  for (i in 1:cols)
+  {
+    column <- data[,i];
+  }
+}
+
 clockData = read.csv("clocks.csv", , ";");
 
 x <- unlist(clockData["X_WGS"]);
 y <- unlist(clockData["Y_WGS"]);
 
-plot(x, y, xlab="", ylab="");
+#plot(x, y, xlab="", ylab="");
 
 xydata = matrix(, length(x), 2);
 
 xydata[, 1] <- x;
 xydata[, 2] <- y;
 
-km <- kmeans(xydata, 12);
+km <- kmeans(xydata, 9);
 
-plotcluster(xydata, km$cluster);
+for (i in 1:length(km$cluster))
+{
+  #points(x[i], y[i], col = km$cluster[i]+1);
+}
+
+hull <- c(3,2);
+hull <- c(hull, c(37.4867430, 55.8153460));
+hull <- c(hull, c(37.4881344, 55.8152801));
+hull <- c(hull, c(37.4934952, 55.8151643));
+
